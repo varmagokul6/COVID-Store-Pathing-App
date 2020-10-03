a42656b75262b1efbe44483cedc0a4e919c3c306
Kaleih <48419153+Kaleih@users.noreply.github.com>
2020-10-03 15:52:04 -0700

    Item Library Generation
    Excel sheet and program to generate a table

--- object_database.m
+++ object_database.m
@@ -1,10 +1,5 @@
 clear
 clc
+itemLibrary = readtable('Item Library.xlsx');
 
-items = ["Milk", "Coffee"];
-
-locations_store1 = ["B1", "C2"];
-
-store_one = containers.Map(items, locations_store1);
-
-
+items = [itemLibrary(:,1)];
