373b6b37811121f1f33d7a6dcb3c2f9b28b5aa8d
chris-whu <60330748+chris-whu@users.noreply.github.com>
2020-10-03 22:45:38 -0700

    Add files via upload

--- store_layout.m
+++ store_layout.m
@@ -1,4 +1,4 @@
-
+clear;clc;
 
 image = imread('storelayout.png');
 
@@ -11,14 +11,53 @@ graph = binaryOccupancyMap(bwimage);
 show(graph);
 hold on
 
-x = checkOccupancy(graph);
+store_matrix = checkOccupancy(graph)
+shelves = find(store_matrix)     %returns matrix indexes where value != 0
 
-y = find(x);
 
-x_coord = 0;
-y_coord = 0;
-hold on
-for i = 1:12
-    plot(x_coord, y_coord, 
+%categorize shelves into horizontal and vertical shelves
+%    if vertical shelf --> check index +- row#
+%    if horizontal shelf --> check index +- 1
+vertical_shelves = [];
+horizontal_shelves = [];
+row_num = size(store_matrix, 1);
+col_num = size(store_matrix, 2);
+for coord = 1:row_num*col_num
+    if store_matrix(coord)==1 && store_matrix(coord+1)==1       %check if shelf is a vertical shelf
+        vertical_shelves = [vertical_shelves;coord];
+    elseif store_matrix(coord)==1 && store_matrix(coord+row_num)==1         %check if shelf is a horizontal shelf
+        horizontal_shelves = [horizontal_shelves;coord];
+    elseif store_matrix(coord)==1 && store_matrix(coord+1)==0 && store_matrix(coord-1)==1      %check for last coordinate of a vertical shelf
+        vertical_shelves = [vertical_shelves;coord];
+    elseif store_matrix(coord)==1 && store_matrix(coord+row_num)==0 && store_matrix(coord-row_num)==1    %check for last coordinate of a horizontal shelf 
+        horizontal_shelves = [horizontal_shelves;coord];
+    end
 end
+
+
+%categorize vertical and horizontal shelves into separate aisles
+%assign Item Library to aisles 
+itemDes = ["A","B","C","D","E","F","G"];
+aisle_random = randi(length(itemDes));
+
+for i = 1:size(vertical_shelves, 2) 
+    if vertical_shelves(i+1)==vertical_shelves(i) + 1
+         
+        
+end
+
+for i = 1:size(horizontal_shelves, 2)
+end
+
+
+
+% for simplification, we assume the aisles are big enough and that vertical shelves will be filled first. 
+
+
+% item_locations = [];
+% store = containers.Map(store_items, item_locations);
+
+
+
+
 
